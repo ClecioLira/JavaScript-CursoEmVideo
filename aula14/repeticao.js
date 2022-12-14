ddd3bd047d956344679b707b701ec1e3bcec545a
Clécio de Lira Alves <cclecioc.com@gmail.com>
2022-12-14 20:00:15 -0300

    add aula 14

---
+++ aula14/repeticao.js
@@ -0,0 +1,16 @@
+/**
+ * REPETIÇÃO COM CONTROLE
+ * let c = 1 //inicio
+ * while (c <= 10) { //teste
+ *  console.log(c)
+ *  c = c + 2 //incrementar
+ * }
+ * 
+ * // for (inicio; teste; incrementar) {
+ * 
+ * }
+ */
+
+for (c = 1; c <= 10; c = c + 2) {
+    console.log(c)
+}
