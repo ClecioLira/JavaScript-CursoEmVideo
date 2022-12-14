e5eb15c0f0098c4925ec1ae925488073297427bd
Clécio de Lira Alves <cclecioc.com@gmail.com>
2022-12-14 19:23:28 -0300

    update

--- aula13/repeticao.js
+++ aula13/repeticao.js
@@ -7,15 +7,15 @@
  */
 
 //TESTE LÓGICO NO INÍCIO
-/*let cont = 0
-while (cont <= 10) {
-    console.log(`Passo ${cont}`)
-    cont++
-} */
+let conta = 0
+while (conta <= 10) {
+    console.log(`Passo ${conta}`)
+    conta++
+}
 
 //TESTE LÓGICO NO FINAL
-/*let cont = 0
+let cont = 0
 do {
     console.log(`Num ${cont}`)
     cont++
-} while (cont <= 10) */
+} while (cont <= 10)
