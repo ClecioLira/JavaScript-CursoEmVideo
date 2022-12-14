606a0d324cf3c0da4022e85dbbcb421986bdb4cf
Clécio de Lira Alves <cclecioc.com@gmail.com>
2022-12-13 22:14:38 -0300

    add aula 13

---
+++ aula13/repeticao.js
@@ -0,0 +1,21 @@
+/**
+ * function comerPizza() {
+ *   while (temFatia) {
+ *      comerFatia()
+ *   }
+ * }
+ */
+
+//TESTE LÓGICO NO INÍCIO
+/*let cont = 0
+while (cont <= 10) {
+    console.log(`Passo ${cont}`)
+    cont++
+} */
+
+//TESTE LÓGICO NO FINAL
+/*let cont = 0
+do {
+    console.log(`Num ${cont}`)
+    cont++
+} while (cont <= 10) */
